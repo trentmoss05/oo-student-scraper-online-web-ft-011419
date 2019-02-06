61aa64a83d5ceafdec672acdbe3c40619be98157
Trent Moss <trentmoss05@gmail.com>
2019-02-06 17:46:53 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,8 +4,7 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    Nokogiri::HTML(open("./fi
-xtures/student-site/index.html"))
+    Nokogiri::HTML(open("./fixtures/student-site/index.html"))
   end
 
   def self.scrape_profile_page(profile_url)
